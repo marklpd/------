6b2ab59d88a4b7e96d907f098cabba25e40cb731
marklpd <2278911462@qq.com>
2022-03-15 11:10:01 +0800

    修改优化

--- js/focus.js
+++ js/focus.js
@@ -1,94 +1,110 @@
-window.addEventListener('load', function () {
-  function animate(obj, target, callback) {
-    obj.style.left = target + 'px';
-    callback && callback();
-  }
-  var arrowl = document.querySelector('.arrowl');
-  var arrowr = document.querySelector('.arrowr');
-  var focus = document.querySelector('.banner-focus');
-  var focusWhidth = focus.offsetWidth;
+window.addEventListener("load", function () {
+  var arrowl = document.querySelector(".arrowl");
+  var arrowr = document.querySelector(".arrowr");
+  var focus = document.querySelector(".banner-focus");
+  var focusWidth = focus.offsetWidth;
   // 动态生成小圆圈
-  var ul = focus.querySelector('ul');
-  var ol = focus.querySelector('.circle');
-  // console.log(ul.children.length);
-  for (var i = 0; i < ul.children.length; i++) {
-    var li = document.createElement('li');
-    //li添加了右浮动，序号相反
-    li.setAttribute('index', ul.children.length - i);
-    ol.appendChild(li);
-    li.addEventListener('click', function () {
-      for (var j = 0; j < ol.children.length; j++) {
-        ol.children[j].className = '';
-      }
-      this.className = 'circle-active';
-      //li 索引号
-      var index = this.getAttribute('index');
-      num = index;
-      circle = index;
-      // console.log(circle);
-      animate(ul, -(index - 1) * focusWhidth);
-
-    })
-  }
-  //第一个圆圈默认样式
-  ol.lastElementChild.className = 'circle-active';
+  var ul = focus.querySelector("ul");
+  var ulChildren = ul.children.length;
+  var ulLi = focus.querySelectorAll("li");
+  var ol = focus.querySelector(".circle");
   //左右按钮计数器
   var num = 1;
-  //控制点击左右按钮时，小圆圈的
+  //控制点击左右按钮时，小圆圈计数器
   var circle = 1;
   // flag 节流阀
   var flag = true;
-  //右侧按钮
-  // console.log(ul.children.length);
-  arrowr.addEventListener('click', function () {
-    num++;
-    if (num > ul.children.length) {
-      num = 1;
-    }
-    // console.log(num);
-    animate(ul, -(num - 1) * focusWhidth);
+  // 自动播放轮播图的定时器 
+  let timer = setInterval(function () {
+    // 手动调用点击事件
+    arrowr.click();
+  }, 3000);
 
-    circle++;
-    if (circle > ol.children.length) {
-      circle = 1;
+  function animate(obj, target, callback) {
+    for (var i = 0; i < obj.length; i++) {
+      obj[i].className="";
     }
-    circleChange();
-  })
-  //左侧按钮
-  arrowl.addEventListener('click', function () {
-    num--;
-    if (num == 0) {
-      num = ul.children.length;
-    }
-    // console.log(num);
-    animate(ul, -(num - 1) * focusWhidth);
-
-    circle--;
-    if (circle == 0) {
-      circle = ul.children.length;
-    }
-    circleChange();
-  })
+    obj[target].className="focus-active";
+    // 配合节流阀
+    setTimeout(() => {
+      callback && callback();
+    }, 1000);
+  }
 
   function circleChange() {
     for (var j = 0; j < ol.children.length; j++) {
-      ol.children[j].className = '';
+      ol.children[j].className = "";
     }
-    ol.children[ol.children.length - circle].className = 'circle-active';
+    ol.children[ol.children.length - circle].className = "circle-active";
   }
-  // 自动播放轮播图
-  var timer = setInterval(function () {
-    //手动调用点击事件
-    arrowr.click();
-  }, 5000);
+
+  const arrListener = (arrow) => {
+    return () => {
+      // 判断节流阀是否开启，开启才执行动画
+      if (flag) {
+        flag = false; // 关闭节流阀
+        // 区分左右按钮
+        arrow === "arrowr" ? num++ : num--;
+        arrow === "arrowr" ? circle++ : circle--;
+        if (num === 0) {
+          num = ulChildren;
+        } else if (num > ulChildren) {
+          num = 1;
+        }
+        if (circle === 0) {
+          circle = ulChildren;
+        } else if (circle > ulChildren) {
+          circle = 1;
+        }
+        // 动画完成后开启节流阀
+        animate(ulLi, num - 1, () => {
+          flag = true;
+        });
+        circleChange();
+      }
+    }
+  }
+  for (var i = 0; i < ulChildren; i++) {
+    var li = document.createElement("li");
+    //li添加了右浮动，序号相反
+    li.setAttribute("index", ulChildren - i);
+    ol.appendChild(li);
+    li.addEventListener("click", function () {
+      //li 索引号
+      var index = this.getAttribute("index");
+      num = index;
+      circle = index;
+      animate(ulLi, index - 1, () => flag = true);
+      circleChange();
+    });
+  }
+  //第一个圆圈默认样式
+  ol.lastElementChild.className = "circle-active";
+  // 右侧按钮
+  arrowr.addEventListener("click", arrListener("arrowr"));
+  // 左侧按钮
+  arrowl.addEventListener("click", arrListener("arrowl"));
+
+  // 鼠标在轮播图上时清除定时器
+  focus.addEventListener('mouseover', () => {
+    clearInterval(timer);
+  })
+  // 鼠标离开轮播图时添加定时器
+  focus.addEventListener('mouseleave', () => {
+    // 自动播放轮播图
+    timer = setInterval(function () {
+      // 手动调用点击事件
+      arrowr.click();
+    }, 3000);
+  })
 
   //鼠标经过显示二维码
-  var wximg = document.querySelector('#wx-img');
-  var wx = document.querySelector('.wx');
-  wx.addEventListener('mouseenter', function() {
-    wximg.style.display = 'block';
-  })
-  wx.addEventListener('mouseleave', function() {
-    wximg.style.display = 'none';
-  })
-})
+  var wximg = document.querySelector("#wx-img");
+  var wx = document.querySelector(".wx");
+  wx.addEventListener("mouseenter", function () {
+    wximg.style.display = "block";
+  });
+  wx.addEventListener("mouseleave", function () {
+    wximg.style.display = "none";
+  });
+});
--- js/index.js
+++ js/index.js
@@ -1,61 +1,48 @@
-window.addEventListener('load', function () {
-// 下拉栏
-
-  var active = document.querySelectorAll('.active');
-  var list =document.querySelectorAll('.header-nav-list');
-  for(var i =0;i<active.length ;i++) {
-    active[i].setAttribute('index', i);
-    active[i].addEventListener('mouseover',function() {
-      var index = this.getAttribute('index');
-      var j = Math.ceil((parseInt(index)+1)/2) -1;
-      list[j].style.height ='229px';
-    })
-    active[i].addEventListener('mouseleave',function() {
-      var index = this.getAttribute('index');
-      var j = Math.ceil((parseInt(index)+1)/2) -1;
-      list[j].style.height ='0';
-    })
-  }
+window.addEventListener("load", function () {
+    // 下拉栏
+    var navItem = document.querySelectorAll(".header-nav-item");
+    var navList = document.querySelectorAll(".header-nav-list");
+    for (var i = 0; i < navItem.length; i++) {
+      navItem[i].setAttribute("index", i);
+      navItem[i].addEventListener("mouseover", function () {
+        var index = this.getAttribute("index");
+        navList[index].className = "header-nav-list active";
+      });
+      navItem[i].addEventListener("mouseleave", function () {
+        var index = this.getAttribute("index");
+        navList[index].className = "header-nav-list";
+      })
+    }
 
   //侧边产品导航栏
-  var navli = document.querySelectorAll('#banner-nav-li');
-  var sidelist = document.querySelectorAll('.banner-nav-sidelist');
-  for (var i = 0;i<navli.length;i++) {
-    navli[i].setAttribute('index', i);
-    navli[i].addEventListener('mouseenter' ,function() {
-      for (var j = 0;j<navli.length;j++ ) {
-        navli[j].className = '';
-      }
-      this.className = 'ff6700';
-      var index = this.getAttribute('index');
-      for (var j = 0;j<sidelist.length;j++ ) {
-        sidelist[j].style.display = 'none';
-      }
-      sidelist[index].style.display = 'block';
+  var navLi = document.querySelectorAll("#banner-nav-li");
+  var sideList = document.querySelectorAll(".banner-nav-sidelist");
+  for (var i = 0; i < navLi.length; i++) {
+    navLi[i].setAttribute("index", i);
+    navLi[i].addEventListener("mouseenter", function () {
+      this.className = "ff6700";
+      var index = this.getAttribute("index");
+      sideList[index].style.display = "block";
     })
-    navli[i].addEventListener('mouseleave' ,function() {
-      this.className = '';
-      var index = this.getAttribute('index');
-      sidelist[index].style.display = 'none';
+    navLi[i].addEventListener("mouseleave", function () {
+      this.className = "";
+      var index = this.getAttribute("index");
+      sideList[index].style.display = "none";
     })
   }
   // 回到顶部
-
-  var main = document.querySelector('main');
-  var backtop = document.querySelector('.backtop');
+  var main = document.querySelector("main");
+  var backTop = document.querySelector(".backtop"); //驼峰命名
   var mainTop = main.offsetTop;
-  // console.log(mainTop);
-  document.addEventListener('scroll', function () {
-    // console.log(window.pageYOffset);
+  document.addEventListener("scroll", function () {
     if (window.pageYOffset > mainTop) {
-      backtop.style.display = 'block';
+      backTop.style.display = "block";
     } else {
-      backtop.style.display = 'none';
+      backTop.style.display = "none";
     }
   })
   //点击返回
-  backtop.addEventListener('click', function() {
-    window.scroll(0,0);
+  backTop.addEventListener("click", function () {
+    window.scroll(0, 0);
   })
-
 })
